c976ee01307d645a50ec880d32b7f3cc7cc0d40b
Adriano Angeli <adrianoangeli01@gmail.com>
2023-06-02 15:46:44 -0300

    Organizing Code

--- app/src/main/java/com/example/appbusinesscard2/MainActivity.kt
+++ app/src/main/java/com/example/appbusinesscard2/MainActivity.kt
@@ -4,34 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Icon
-import androidx.compose.material3.LinearProgressIndicator
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.paint
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -56,38 +45,80 @@ fun AppBusinessAppPreview(){
 
 @Composable
 fun AppBusinessApp(){
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier
-            .background(Color(7, 48, 66))
-            .fillMaxSize()
-            .padding(top = 150.dp)
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .background(Color(7, 48, 66))
     ) {
-            Icon(
-                painter = painterResource(id = R.drawable.android_logo),
-                contentDescription = null,
-                tint = Color.Unspecified,
-                modifier = Modifier.size(120.dp)
-            )
-            Text(
-                text = "Adriano Angeli",
-                color = Color.White,
-                fontSize = 42.sp
-            )
-            Text(
-                text = "Android Developer",
-                color = Color(color = 0xFF3ddc84),
-                fontSize = 18.sp,
-                fontWeight = FontWeight.Bold
-            )
+        InfoImageNameAndTitle(
+            icon = painterResource(id = R.drawable.android_logo),
+            name = stringResource(R.string.text_name),
+            title = stringResource(R.string.text_title),
+            modifier = Modifier
+        )
     }
 
+    Column(modifier = Modifier.fillMaxSize()) {
+        InfoPhoneSocialAndEmail(
+            phoneIcon = painterResource(id = R.drawable.baseline_local_phone_24),
+            phone = stringResource(R.string.text_phone),
+            socialIcon = painterResource(id = R.drawable.baseline_share_24),
+            social = stringResource(R.string.text_social_media),
+            emailIcon = painterResource(id = R.drawable.baseline_email_24),
+            email = stringResource(R.string.text_email)
+        )
+    }
+
+}
+
+@Composable
+fun InfoImageNameAndTitle(
+    icon: Painter,
+    name: String,
+    title: String,
+    modifier: Modifier
+){
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 150.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Icon(
+            painter = icon,
+            contentDescription = null,
+            tint = Color.Unspecified,
+            modifier = modifier.size(120.dp)
+        )
+        Text(
+            text = name,
+            color = Color.White,
+            fontSize = 42.sp
+        )
+        Text(
+            text = title,
+            color = Color(color = 0xFF3ddc84),
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
+
+@Composable
+fun InfoPhoneSocialAndEmail(
+    phoneIcon: Painter,
+    phone: String,
+    socialIcon: Painter,
+    social: String,
+    emailIcon: Painter,
+    email: String
+){
     Column(
         horizontalAlignment = Alignment.Start,
         verticalArrangement = Arrangement.Bottom,
         modifier = Modifier
             .fillMaxSize()
-            //.padding(start = 66.dp, bottom = 22.dp)
+        //.padding(start = 66.dp, bottom = 22.dp)
     ) {
 
 
@@ -109,13 +140,13 @@ fun AppBusinessApp(){
                 .padding(top = 15.dp)
         ) {
             Icon(
-                painter = painterResource(id = R.drawable.baseline_local_phone_24),
+                painter = phoneIcon,
                 contentDescription = null,
                 tint = Color(color = 0xFF3ddc84),
                 modifier = Modifier.padding(start = 60.dp)
             )
             Text(
-                text = "+55 (19) 99999-9999",
+                text = phone,
                 color = Color.White,
                 modifier = Modifier.padding(start = 16.dp, bottom = 16.dp)
             )
@@ -139,13 +170,13 @@ fun AppBusinessApp(){
                 .padding(top = 15.dp)
         ) {
             Icon(
-                painter = painterResource(id = R.drawable.baseline_share_24),
+                painter = socialIcon,
                 contentDescription = null,
                 tint = Color(color = 0xFF3ddc84),
                 modifier = Modifier.padding(start = 60.dp)
             )
             Text(
-                text = "@dri.angeli",
+                text = social,
                 color = Color.White,
                 modifier = Modifier.padding(start = 16.dp, bottom = 16.dp)
             )
@@ -169,17 +200,16 @@ fun AppBusinessApp(){
                 .padding(top = 10.dp, bottom = 46.dp)
         ) {
             Icon(
-                painter = painterResource(id = R.drawable.baseline_email_24),
+                painter = emailIcon,
                 contentDescription = null,
                 tint = Color(color = 0xFF3ddc84),
                 modifier = Modifier.padding(start = 60.dp)
             )
             Text(
-                text = "adriano@android.com",
+                text = email,
                 color = Color.White,
                 modifier = Modifier.padding(start = 16.dp)
             )
         }
     }
-
 }
